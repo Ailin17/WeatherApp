602561d0ac9ca1d80e10739f55668b4e44e837cb
Ailin Pedrossian <Apedrossian17@gmail.com>
2020-08-05 14:08:58 +1000

    https

--- src/weather.js
+++ src/weather.js
@@ -5,7 +5,7 @@
     }
 
     async getWeather() {
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?id=${this.city}&appid=${this.apiKey}`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=${this.city}&appid=${this.apiKey}`);
 
         const responseData = await response.json();
 
